2ea82387b049c6350001bf0628f9318dd4131a2a
sunlliu <sunlliu@tencent.com>
2023-10-23 19:46:15 +0800

    feat: node版本管理器n

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -16,7 +16,8 @@ export default defineConfig({
         text: '项目',
         items: [
           { text: '小程序', link: '/docs/项目/小程序' },
-          { text: 'vue', link: '/docs/项目/vue' },
+          { text: '项目生成器', link: '/docs/项目/项目生成器' },
+          { text: '圆环倒计时', link: '/docs/项目/倒计时圆环' },
           { text: 'bug', link: '/docs/项目/bug' },
         ]
       },
@@ -26,6 +27,7 @@ export default defineConfig({
           { text: '环境配置', link: '/docs/配置/环境配置' },
           { text: '其他', link: '/docs/配置/其他' },
           { text: '部署', link: '/docs/配置/部署' },
+          { text: 'git', link: '/docs/配置/git指令' },
 
         ]
       },
---
+++ docs/配置/git指令.md
@@ -0,0 +1,10 @@
+## 本地已有仓库链接远程仓库
+1. 连接 `git remote add origin https://github.com/xxx/xxxxxx.git`
+2. 推到远程仓库 `git push --set-upstream origin main -f`
+
+## 创建并切换到一个新的分支
+`git checkout -b feat/xxx`
+
+## 修改分支名
+`git branch -m new-branch`
+

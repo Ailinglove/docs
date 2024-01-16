65e0259a014bd44e7c3d96b2816c96ad35c91ed4
sunlliu <sunlliu@tencent.com>
2024-01-16 10:37:54 +0800

    feat: 根据路径的不同展示不同的侧边栏

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -8,50 +8,58 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: '项目', link: '/docs/项目/小程序' },
-      { text: 'Vue', link: '/docs/Vue/指令' },
+      { text: '记录', link: '/docs/项目/小程序' },
+      { text: 'interview', link: '/interview/基础' },
 
-      { text: '问题解决', link: '/docs/配置/环境配置' },
     ],
-    sidebar: [
-      {
-        text: '项目',
-        items: [
-          { text: '小程序', link: '/docs/项目/小程序' },
-          { text: '圆环倒计时', link: '/docs/项目/倒计时圆环' },
-        ]
-      },
-      {
-        text: 'Vue',
-        items: [
-          { text: '项目生成器', link: '/docs/项目/项目生成器' },
-          { text: '从0开始写一个v-loading指令', link: '/docs/Vue/指令' },
-          { text: '从0开始写一个vite插件', link: '/docs/Vue/consolePlugin插件' },
-          { text: '抽象语法树', link: '/docs/Vue/抽象语法树AST' },
-          { text: 'webpack', link: '/docs/Vue/webpack' },
-          { text: 'css扩展器', link: '/docs/Vue/css扩展器' },
-
-        ]
-      },
-      {
-        text: '配置',
-        items: [
-          { text: '环境配置', link: '/docs/配置/环境配置' },
-          { text: '其他', link: '/docs/配置/其他' },
-          { text: '部署', link: '/docs/配置/部署' },
-          { text: 'git', link: '/docs/配置/git指令' },
-        ]
-      },
-      {
-        text: '问题解决',
-        items: [
-          { text: '问题', link: '/docs/问题解决/READEME' },
-        
-        ]
-      },
-      
-    ],
-
+    sidebar:{
+      '/docs':[
+        {
+          text: '项目',
+          items: [
+            { text: '小程序', link: '/docs/项目/小程序' },
+            { text: '圆环倒计时', link: '/docs/项目/倒计时圆环' },
+          ]
+        },
+        {
+          text: 'Vue',
+          items: [
+            { text: '项目生成器', link: '/docs/项目/项目生成器' },
+            { text: '从0开始写一个v-loading指令', link: '/docs/Vue/指令' },
+            { text: '从0开始写一个vite插件', link: '/docs/Vue/consolePlugin插件' },
+            { text: '抽象语法树', link: '/docs/Vue/抽象语法树AST' },
+            { text: 'webpack', link: '/docs/Vue/webpack' },
+            { text: 'css扩展器', link: '/docs/Vue/css扩展器' },
+          ]
+        },
+        {
+          text: '配置',
+          items: [
+            { text: '环境配置', link: '/docs/配置/环境配置' },
+            { text: '其他', link: '/docs/配置/其他' },
+            { text: '部署', link: '/docs/配置/部署' },
+            { text: 'git', link: '/docs/配置/git指令' },
+          ]
+        },
+        {
+          text: '问题解决',
+          items: [
+            { text: '问题', link: '/docs/问题解决/READEME' },
+          ]
+        },      
+      ],
+      '/interview/':[
+        {
+          text: 'interview',
+          items: [
+            { text: '基础', link: '/interview/基础' },
+          ]
+        },
+      ]
+    } ,
+    rewrites: {
+      'interview/(.*)': ':pkg/基础.md'
+    },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     ]
